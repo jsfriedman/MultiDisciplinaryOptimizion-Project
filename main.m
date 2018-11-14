a190815785869ffdd6e7ec810bdfda7326fb0eda
jsfriedman <friedman.jonny@gmail.com>
2018-11-14 13:38:01 +0100

    main file

---
+++ main.m
@@ -0,0 +1,22 @@
+% Jonathan Friedman 4799380
+% Jackson Horwitz [INSERT]
+% AE4??? MDO for Aerospace, Final Project, 2018Q1
+
+% This is intended to be the main file of the program. This might change as
+% we go along.
+
+% Run this file
+
+%% Initial Values (including "guess" variables
+
+
+%% Design Variable
+
+
+%% Bounds
+
+
+%% Initial Constraint Values
+
+
+%% 
